803f78cce3ffae3eae190663fe1b828b52c0a797
Martin M Venturino <marventurino@gamil.com.ar>
2024-02-12 16:22:57 -0300

    4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::{thread, str};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -12,7 +12,7 @@ fn main() {
             Ok(_stream) => {
                 println!("accepted new connection"); 
                 thread::spawn(move || {
-                    handle_client(_stream);
+                    let _ = handle_client(_stream);
                 });
             }
             Err(e) => {
@@ -20,18 +20,41 @@ fn main() {
             }
         }
     }
+    Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
     let mut buf = [0; 512];
 
-    // read from the stream
     loop {
         let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
         if bytes_read == 0 {
-            return;
+            return Ok(());
         }
-        stream.write_all(b"+PONG\r\n").expect("Failed to write to client");
+        let s = str::from_utf8(&buf[..bytes_read])?;
+        let parts = s.split("\r\n").collect::<Vec<_>>();
+
+        print!("{:?}", parts);
+
+        if parts.len() >= 3 && parts[0].starts_with('*') {
+            match parts[2].to_ascii_lowercase().as_ref() {
+                "ping" => {
+                    let response = "+PONG\r\n";
+                    stream.write_all(response.as_bytes())?;
+                    stream.flush()?;
+                }
+                "echo" if parts.len() >= 5 => {
+                    let response = format!("${}\r\n{}\r\n", parts[4].len(), parts[4]);
+                    stream.write_all(response.as_bytes())?;
+                    stream.flush()?;
+                }
+                _ => {
+                    // Response with null
+                    stream.write_all("-1\r\n".as_bytes())?;
+                    stream.flush()?
+                }
+            }
+        }
     }
 }
 
