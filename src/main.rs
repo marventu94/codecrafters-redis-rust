d96123284f16457ef2875ae0fa853e87e32f7838
Martin M Venturino <marventurino@gamil.com.ar>
2024-02-19 16:04:18 -0300

    psync response 5

--- src/main.rs
+++ src/main.rs
@@ -179,7 +179,7 @@ fn handle_client(mut stream: TcpStream, server: Arc<Mutex<Server>>) -> anyhow::R
                     let _ = stream.flush();
                 },
                 "psyc" => {
-                    let msg = "+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0";
+                    let msg = "+FULLRESYNC8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb0";
                     let msg_reso = format!("${}\r\n{}\r\n", msg.len(), msg);
                     let _ = stream.write_all(msg_reso.as_bytes());
                     stream.flush()?
